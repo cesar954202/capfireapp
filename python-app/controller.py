829c8944331b19181cfd6f071998dd6805ae212e
cesar954202 <cesar95.4202@gmail.com>
2020-09-30 21:20:25 -0500

    Infrestructura terminada

--- python-app/controller.py
+++ python-app/controller.py
@@ -1,1 +1,9 @@
-import cv2
+# -*- coding: utf-8 -*-
+
+
+def application(environ, start_response):
+    output = "<p>Test web python</p>"
+
+    start_response('200 OK',[('Content-Type', 'text/html; charset=utf-8' )])
+
+    return output
